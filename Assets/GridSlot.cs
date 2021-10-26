eed79cdbe1c8ca3e3799ff4232346e9bc9a3c668
predator1245 <jashshah37@yahoo.com>
2021-10-26 17:43:17 +0200

    Add items to the Container

--- Assets/GridSlot.cs
+++ Assets/GridSlot.cs
@@ -6,6 +6,8 @@ public class GridSlot : MonoBehaviour
 {
     public float x = -1;
     public float y = -1;
+    public int x_idx = -1;
+    public int y_idx = -1;
     public bool occupied = false;
     public Item item = null;
 
--- Assets/ItemSlotGridDimensioner.cs
+++ Assets/ItemSlotGridDimensioner.cs
@@ -23,6 +23,8 @@ public class ItemSlotGridDimensioner : MonoBehaviour
     public bool isContainer = false;
     
     private LinkedList<GameObject> items;
+    private float invert = 1;
+    Vector3 gridOffset = new Vector3(20, -20, 0);
 
     void Start()
     {
@@ -32,20 +34,28 @@ public class ItemSlotGridDimensioner : MonoBehaviour
 
         slotArray = new GridSlot[GridDimensions.x, GridDimensions.y];
 
-        for (int x_cell = 0; x_cell < GridDimensions.x; x_cell++)
+        for (int y_cell = 0; y_cell < GridDimensions.y; y_cell++)
         {
-            for (int y_cell = 0; y_cell < GridDimensions.y; y_cell++)
+            for (int x_cell = 0; x_cell < GridDimensions.x; x_cell++)
             {
                 GameObject newObject = Instantiate(itemSlotPrefab, this.transform);
                 newObject.GetComponent<GridSlot>().x = x_cell * width + width / 2;
-                newObject.GetComponent<GridSlot>().y = y_cell * height + width / 2;
+
+                if (isContainer)
+                {
+                    newObject.GetComponent<GridSlot>().x = (GridDimensions.x - 1 - x_cell) * width + width / 2;
+                }
+                newObject.GetComponent<GridSlot>().y = y_cell * height + height / 2;
+                newObject.GetComponent<GridSlot>().x_idx = x_cell;
+                newObject.GetComponent<GridSlot>().y_idx = y_cell;
                 slotArray[x_cell, y_cell] = newObject.GetComponent<GridSlot>();
             }
         }
 
         if (isContainer)
         {
-            return;
+            invert = -1f;
+            gridOffset.x = gridOffset.x * (-1f);
         }
 
         items = new LinkedList<GameObject>();
@@ -58,7 +68,6 @@ public class ItemSlotGridDimensioner : MonoBehaviour
     private void AddPrefab(string name, int x, int y)
     {
         // Instantiate object
-        Vector3 gridOffset = new Vector3(20, -20, 0);
         Object prefab = AssetDatabase.LoadAssetAtPath(name, typeof(GameObject));
         GameObject obj = Instantiate(prefab, this.transform) as GameObject;
 
@@ -68,9 +77,22 @@ public class ItemSlotGridDimensioner : MonoBehaviour
         Vector3 obj_offset = new Vector3(obj_width * width, - obj_height * height, 0);
 
         // Transform object to the correct spot
-        obj.transform.localPosition = new Vector3(slotArray[x, y].x, -slotArray[x, y].y, 0) + gridOffset + obj_offset;
-        
+        obj.transform.localPosition = new Vector3(invert * slotArray[x, y].x, -slotArray[x, y].y, 0) + gridOffset + obj_offset;
+        SetOccupied(x, y, obj.GetComponent<ItemSlot>().itemInSlot.width, obj.GetComponent<ItemSlot>().itemInSlot.height);
+
         items.AddLast(obj);
+        Debug.Log(name + " added");
     }
 
+    private void SetOccupied(int x, int y, int obj_width, int obj_height)
+    {
+        for (int i = x; i < x + obj_width; i++)
+        {
+            for (int j = y; j < y + obj_height; j++)
+            {
+                Debug.Log("Set " + i + " , " + j + " occupied");
+                slotArray[i, j].occupied = true;
+            }
+        }
+    }
 }
